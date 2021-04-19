9d063245e694ce5072c32fdc1c49ba3a1f972d79
Jim6699 <81064597+Jim6699@users.noreply.github.com>
2021-04-19 09:23:09 +0800

    Update WPS_accept_invitation.py

--- WPS_accept_invitation.py
+++ WPS_accept_invitation.py
@@ -32,13 +32,13 @@ def request_re(sid, invite_userid, rep = 30):
 
 for i in invite_userids:
     for j in sids:
-    try:
-    r = request_re(j, i)
-    js = json.loads(r.content)
-    if js['result'] == 'ok':
-    mk += 1
-    except:
-    print('发生未知错误')
+        try:
+            r = request_re(j, i)
+            js = json.loads(r.content)
+            if js['result'] == 'ok':
+            mk += 1
+        except:
+            print('发生未知错误')
                
 
 SERVER_KEY = os.getenv('SERVER_KEY')
